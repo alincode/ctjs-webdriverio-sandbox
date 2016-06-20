282172f0382bd17d6b5a250a1ba3f2540e46d74b
alincode <alin.code@gmail.com>
2016-06-20 22:34:36 +0800

    for easy demo

--- test/specs/test.js
+++ test/specs/test.js
@@ -4,6 +4,8 @@ describe('mokayo page', function() {
   it('should have the right title', function() {
     browser.url('http://blog.mokayo.com');
     var title = browser.getTitle();
+    // console.log('title:', title);
+    // browser.pause(5000);
     assert.equal(title, '教你所想學的，用眼樂讀 - blog.mokayo.com');
   });
 });
